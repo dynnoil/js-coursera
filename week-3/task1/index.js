c9f44ec8a69c60d8ded665f6b754eac1a9beef82
Крюков Леонид <LYuKrukov.SBT@sberbank.ru>
2018-07-23 18:45:52 +0300

    Пытаюсь решить

--- week-3/task1/index.js
+++ week-3/task1/index.js
@@ -1,11 +1,38 @@
+var UNITS_OF_TIME = ['years', 'months', 'days', ' hours', 'minutes'];
+
 var checkUnitOfTime = function (unitOfTime) {
-    return /(years|months|days|hours|minutes)/i.test(unitOfTime);
+    return /^(years|months|days|hours|minutes)$/im.test(unitOfTime);
 }
 
 var checkMethodInputs = function (number, unitOfTime) {
-    if (number < 0 && !checkUnitOfTime(unitOfTime)) {
-        throw new TypeError('Unknown unit of time');
+    if (number < 0 || !checkUnitOfTime(unitOfTime)) {
+        throw new TypeError('Negative value or Unknown unit of time');
     }
+}
+
+var getUnitsValues = function (date) {
+    var matchedDate = date.match(/(\d{4})-(\d{2})-(\d{2})\s(\d{2}):(\d{2})/i);
+    return {
+        years: Number(matchedDate[1]),
+        months: Number(matchedDate[2]),
+        days: Number(matchedDate[3]),
+        hours: Number(matchedDate[4]),
+        minutes: Number(matchedDate[5])
+    };
+}
+
+var wrapDateUnit = function (unit) {
+    return unit < 10 ? 0 + String(unit) : unit;
+}
+
+var parseDate = function (date) {
+    return String(
+        date.getFullYear() + '-' +
+        wrapDateUnit(date.getMonth()) + '-' +
+        wrapDateUnit(date.getDay()) + ' ' +
+        wrapDateUnit(date.getHours()) + ':' +
+        wrapDateUnit(date.getMinutes())
+    );
 }
 
 /**
@@ -13,51 +40,86 @@ var checkMethodInputs = function (number, unitOfTime) {
  * @returns {Object}
  */
 module.exports = function (date) {
-    var matchedDate = date.match(/\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}/);
-    // var year = matchedDate[0];
-    // var month = matchedDate[1];
-    // var day = matchedDate[2];
-    // var hours = matchedDate[3];
-    // var minutes = matchedDate[4];
+    var matchedDate = date.match(/(\d{4})-(\d{2})-(\d{2})\s(\d{2}):(\d{2})/i);
+    var years = matchedDate[1];
+    var months = matchedDate[2];
+    var days = matchedDate[3];
+    var hours = matchedDate[4];
+    var minutes = matchedDate[5];
 
     return {
-        value: new Date(date),
-        matchedDate: date.match(/\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}/),
+        value: date,
+        // _years: Number(years),
+        // _months: Number(months),
+        // _days: Number(days),
+        // _hours: Number(hours),
+        // _minutes: Number(minutes),
+
+        // fill: function (date) {
+        //     UNITS_OF_TIME.forEach(function (unit) {
+        //         this[unit] = date[unit];
+        //     });
+        // },
 
         add: function (number, unitOfTime) {
             checkMethodInputs(number, unitOfTime);
+            var unitsValues = getUnitsValues(this.value);
+            var date = new Date(
+                unitsValues.years,
+                unitsValues.months - 1,
+                unitsValues.days,
+                unitsValues.hours,
+                unitsValues.minutes
+            );
             switch (unitOfTime) {
                 case 'years':
-                    this.value.setYear();
+                    date.setFullYear(unitsValues.years + number);
+                    break;
                 case 'months':
-                    month += number;
+                    date.setMonth(unitsValues.months + number);
+                    break;
                 case 'days':
-                    day += number;
+                    date.setDate(unitsValues.days + number);
+                    break;
                 case 'hours':
-                    ;
+                    date.setHours(unitsValues.hours + number);
+                    break;
                 case 'minutes':
-                    this.value.setMinutes(this.value.getMinutes() + number);
+                    date.setMinutes(unitsValues.minutes + number);
+                    break;
             }
+            this.value = parseDate(date);
             return this;
         },
         subtract: function (number, unitOfTime) {
             checkMethodInputs(number, unitOfTime);
+            var unitsValues = getUnitsValues(this.value);
+            var date = new Date(
+                unitsValues.years,
+                unitsValues.months,
+                unitsValues.days,
+                unitsValues.hours,
+                unitsValues.minutes
+            );
             switch (unitOfTime) {
                 case 'years':
-                    year -= number;
+                    date.setFullYear(unitsValues.years - number);
+                    break;
                 case 'months':
-                    month -= number;
+                    date.setMonth(unitsValues.months - number);
+                    break;
                 case 'days':
-                    day -= number;
+                    date.setDate(unitsValues.days - number);
+                    break;
                 case 'hours':
-                    hours -= number;
+                    date.setHours(unitsValues.hours - number);
+                    break;
                 case 'minutes':
-                    this.value.setMinutes(this.value.getMinutes() - number);
+                    date.setMinutes(unitsValues.minutes - number);
+                    break;
             }
+            this.value = parseDate(date);
             return this;
-        },
-        toString: function () {
-            return String(this.value);
         }
     }
 };
